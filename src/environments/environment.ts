a063b5cede00261de99e7800893d09e7f38e57ac
Nacho Gadea <nachogp17@gmail.com>
2020-12-27 13:33:27 +0100

    firebase connection

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,14 +7,13 @@ export const environment = {
 
 
   firebase: {
-    apiKey: "AIzaSyCVOM6fAAgoOrLDNJJRa6Y0OasWZlQcNS0",
-    authDomain: "tutorialfirebase-f3faf.firebaseapp.com",
-    databaseURL: "https://tutorialfirebase-f3faf.firebaseio.com",
-    projectId: "tutorialfirebase-f3faf",
-    storageBucket: "tutorialfirebase-f3faf.appspot.com",
-    messagingSenderId: "610608193158",
-    appId: "1:610608193158:web:f323aec320992fdd7c7b07",
-    measurementId: "G-TX4RG4KKGR"
+    apiKey: "AIzaSyDdVES8FThkAEJxwkgAk45fW6vZy3SmDEo",
+    authDomain: "fir-tutorial-eb0a4.firebaseapp.com",
+    projectId: "fir-tutorial-eb0a4",
+    storageBucket: "fir-tutorial-eb0a4.appspot.com",
+    messagingSenderId: "49071413770",
+    appId: "1:49071413770:web:5efe0d80b32fa37b575df3",
+    measurementId: "G-365FNJBVYR"
   }
 
 
